4f2ed06fbab09cee95d1db1e4e8db1266bed87c7
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2024-02-08 14:23:14 +0200

    Add Bootstrap

--- ui/currency-converter-ui/src/app/app.component.ts
+++ ui/currency-converter-ui/src/app/app.component.ts
@@ -4,7 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `<h1>Welcome to {{title}}! Diplom</h1>`,
+  template: `
+  <nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+  `,
   styles: [],
 })
 export class AppComponent {
