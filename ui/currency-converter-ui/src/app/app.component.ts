cf0f92e2a48a6dc56e0b9af69d265f6c161f35b4
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2024-02-09 10:36:11 +0200

    Npm bootstrap, bootstrap-icons

--- ui/currency-converter-ui/src/app/app.component.ts
+++ ui/currency-converter-ui/src/app/app.component.ts
@@ -4,18 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   standalone: true,
   imports: [],
-  template: `
-  <nav class="navbar bg-body-tertiary">
-  <div class="container-fluid">
-    <form class="d-flex" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-  `,
-  styles: [],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'currency-converter-ui';
+  
 }
