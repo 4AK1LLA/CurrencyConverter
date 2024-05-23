be603390e12eced3ab8b574a38169d8b99acfca1
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2024-05-23 17:49:54 +0300

    Currency and Rate entities

---
+++ api/CurrencyConverterAPI/Entity/Currency.cs
@@ -0,0 +1,9 @@
+﻿namespace CurrencyConverterAPI.Entity
+{
+    public class Currency
+    {
+        public string? DisplayName { get; set; }
+        public string? Description { get; set; }
+        public char Symbol { get; set; }
+    }
+}
---
+++ api/CurrencyConverterAPI/Entity/Rate.cs
@@ -0,0 +1,9 @@
+﻿namespace CurrencyConverterAPI.Entity
+{
+    public class Rate
+    {
+        public double RateToBase { get; set; }
+        public DateTime Date { get; set; }
+        public Currency? Currency { get; set; }
+    }
+}
