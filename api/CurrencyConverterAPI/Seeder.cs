fb317170a93514fdaf304dd18b1046a80e3f8705
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2024-05-24 13:13:35 +0300

    Rates seed

--- api/CurrencyConverterAPI/Seeder.cs
+++ api/CurrencyConverterAPI/Seeder.cs
@@ -1,6 +1,7 @@
 ﻿using CurrencyConverterAPI.Entity;
 using YamlDotNet.Serialization;
 using YamlDotNet.Serialization.NamingConventions;
+using YamlDotNet.Serialization.Converters;
 
 namespace CurrencyConverterAPI
 {
@@ -17,6 +18,7 @@ namespace CurrencyConverterAPI
 
             var deserializer = new DeserializerBuilder()
                 .WithNamingConvention(HyphenatedNamingConvention.Instance)
+                .WithTypeConverter(new DateTimeConverter(formats: "dd.MM.yyyy HH:mm:ss"))
                 .Build();
 
             using (var reader = new StreamReader(filePath))
@@ -33,6 +35,29 @@ namespace CurrencyConverterAPI
                         Symbol = currency.Value.Symbol
                     });
                 }
+
+                context.SaveChanges();
+
+                foreach (var rate in yaml.Rates)
+                {
+                    foreach (var value in rate.Values)
+                    {
+                        var currency = context.Currencies!.FirstOrDefault(c => c.Code == value.Key);
+
+                        if (currency == null)
+                        {
+                            logger.LogError($"Currency with code {value.Key} not found");
+                            continue;
+                        }
+
+                        context.Rates!.Add(new Rate()
+                        {
+                            Currency = currency,
+                            Date = rate.Date,
+                            RateToBase = value.Value
+                        });
+                    }
+                }
             }
 
             context.SaveChanges();
@@ -43,12 +68,19 @@ namespace CurrencyConverterAPI
     public class YamlModel
     {
         public Dictionary<string, CurrencyDetail> Currencies { get; set; }
+        public List<RateDetail> Rates { get; set; }
 
         public class CurrencyDetail
         {
             public string DisplayName { get; set; }
             public string Description { get; set; }
             public char Symbol { get; set; }
+        }
+
+        public class RateDetail
+        {
+            public DateTime Date { get; set; }
+            public Dictionary<string, double> Values { get; set; }
         }
     }
 }
