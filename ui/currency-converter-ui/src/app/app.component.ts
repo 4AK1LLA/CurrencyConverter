5514afbf5df95d27e85989cd16a4a665afea9512
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2024-05-25 14:43:01 +0300

    Convert component

--- ui/currency-converter-ui/src/app/app.component.ts
+++ ui/currency-converter-ui/src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ConvertComponent } from '../components/convert/convert.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [ ConvertComponent ],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
---
+++ ui/currency-converter-ui/src/components/convert/convert.component.ts
@@ -0,0 +1,47 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convert',
+  standalone: true,
+  imports: [ CommonModule ],
+  templateUrl: 'convert.component.html',
+  styleUrl: 'convert.component.css'
+})
+export class ConvertComponent {
+  currencies: Currency[];
+  from: Currency;
+  to: Currency;
+
+  ngOnInit() {
+    this.currencies = [
+      new Currency(1, "USD", "US Dollar", "", "$"),
+      new Currency(2, "EUR", "Euro", "", "€"),
+      new Currency(3, "UAH", "Ukrainian Hryvnia", "", "₴"),
+      new Currency(4, "BTC", "Bitcoin", "", "₿")
+    ];
+
+    this.from = this.currencies[0];
+    this.to = this.currencies[1];
+  }
+
+  swapCurrencies() {
+    [this.from, this.to] = [this.to, this.from];
+  }
+}
+
+class Currency {
+  id: number;
+  code: string;
+  displayName: string;
+  description: string;
+  symbol: string;
+
+  constructor(id:number, code: string, displayName: string, description: string, symbol: string) {
+    this.id = id;
+    this.code = code;
+    this.displayName = displayName;
+    this.description = description;
+    this.symbol = symbol;
+  }
+}
