5092b586513d43b484bc0bc67f562a07c122e80f
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2024-05-26 19:18:27 +0300

    Currencies information

--- ui/currency-converter-ui/src/components/convert/convert.component.ts
+++ ui/currency-converter-ui/src/components/convert/convert.component.ts
@@ -28,8 +28,8 @@ export class ConvertComponent {
 
   ngOnInit() {
     this.currencies = [
-      new Currency(1, "USD", "US Dollar", "", "$"),
-      new Currency(2, "EUR", "Euro", "", "€"),
+      new Currency(1, "USD", "US Dollar", "The US Dollar is the official currency of the United States and its territories. It is the world's primary reserve currency and the most widely used currency in international transactions. Introduced in 1792, the USD is known for its stability and widespread acceptance.", "$"),
+      new Currency(2, "EUR", "Euro", "The Euro is the official currency of the Eurozone, which consists of 19 of the 27 member states of the European Union. Introduced in 1999, it is the second most traded currency in the world and is used by over 340 million Europeans.", "€"),
       new Currency(3, "UAH", "Ukrainian Hryvnia", "", "₴"),
       new Currency(4, "BTC", "Bitcoin", "", "₿"),
       new Currency(5, "JPY", "Japanese Yen", "", "¥"),
