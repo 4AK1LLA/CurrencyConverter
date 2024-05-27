da67f59974c02cc277caabca721b31a98de0b74b
4AK1LLA <ruslan.tretyakov2003@gmail.com>
2024-05-27 15:03:20 +0300

    Add environment

--- ui/currency-converter-ui/src/resolvers/currency.resolver.ts
+++ ui/currency-converter-ui/src/resolvers/currency.resolver.ts
@@ -3,8 +3,9 @@ import { ResolveFn } from '@angular/router';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Currency } from '../models/currency';
+import { environment } from '../environments/environment';
 
 export const currencyResolver: ResolveFn<Currency[]> = (): Observable<Currency[]> => {
     const http = inject(HttpClient);
-    return http.get<Currency[]>('https://localhost:7293/currencies');
+    return http.get<Currency[]>(environment.apiUrl + '/currencies');
 }
